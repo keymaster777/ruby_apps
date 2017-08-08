832c6a1f760ada01cf1d3bc5851d016e2eb11198
Riley <rileykeycleary@yahoo.com>
2017-08-08 16:38:42 -0700

    setup routes for all three apps

--- app.rb
+++ app.rb
@@ -5,3 +5,15 @@ require 'sinatra/reloader' if development?
 get '/' do
 	erb :index
 end
+
+get '/caesar-cipher' do
+	erb :caesar_cipher
+end
+
+get '/hangman' do
+	erb :hangman
+end
+
+get '/tic-tac-toe' do
+	erb :tic_tac_toe
+end
