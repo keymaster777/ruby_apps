6aca0c43c8fce66cbf1831bd3e6e6a884bfb1c2c
Riley <rileykeycleary@yahoo.com>
2017-08-09 10:41:00 -0700

    Cipher page finished, needs more styling

--- app.rb
+++ app.rb
@@ -7,6 +7,9 @@ get '/' do
 end
 
 get '/caesar-cipher' do
+	@message = params["message"]
+	@shift = params["shift"].to_i
+	@coded_message=cipher(@message,@shift)
 	erb :caesar_cipher
 end
 
@@ -16,4 +19,30 @@ end
 
 get '/tic-tac-toe' do
 	erb :tic_tac_toe
+end
+
+def cipher(input,shift)
+	original_input=input.split("")
+	input_ord=original_input.map {|i| i.ord}
+	input_length=input_ord.length
+	output=Array.new
+	for i in 0...input_length
+		if input_ord[i].between?(65,90)
+			if input_ord[i] + shift > 90
+				input_ord[i]-=26
+			elsif input_ord[i] + shift < 65
+				input_ord[i]+=26
+			end
+			input_ord[i]+=shift
+		elsif input_ord[i].between?(97,122)
+			if input_ord[i] + shift > 122
+				input_ord[i]-=26
+			elsif input_ord[i] + shift < 97
+				input_ord[i]+=26
+			end
+			input_ord[i]+=shift
+		end
+		output << input_ord[i].chr
+	end
+output.join("")	
 end
